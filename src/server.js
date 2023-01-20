80c9f5d7c0c5e0d97db1f7e0f2364bfc026e898f
xiomay <xiomay@local>
2023-01-20 16:18:24 +0700

    server-side caching implemented

--- src/server.js
+++ src/server.js
@@ -3,7 +3,7 @@ require('dotenv').config();
 const Hapi = require('@hapi/hapi');
 const Jwt = require('@hapi/jwt');
 const Inert = require('@hapi/inert');
-const path = require('path');
+// const path = require('path');
 
 const notes = require('./api/notes');
 const NotesService = require('./services/postgres/NotesService');
@@ -36,11 +36,16 @@ const uploads = require('./api/uploads');
 const StorageService = require('./services/S3/StorageService');
 const UploadsValidator = require('./validator/uploads');
 
+// cache
+const CacheService = require('./services/redis/CacheService');
+
 const init = async () => {
-  const collaborationsService = new CollaborationsService();
-  const notesService = new NotesService(collaborationsService);
+  const cacheService = new CacheService();
+  const collaborationsService = new CollaborationsService(cacheService);
+  const notesService = new NotesService(collaborationsService,cacheService);
   const usersService = new UsersService();
   const authenticationsService = new AuthenticationsService();
+  // eslint-disable-next-line max-len
   // const storageService = new StorageService(path.resolve(__dirname, 'api/uploads/file/images')); //local
   const storageService = new StorageService();
 
